7ba2df4403ed60503131696dad063a6735c2e271
Matvii Voznosimenko <main.monyster@gmail.com>
2022-09-18 21:09:10 +0300

    navbar panel added

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,30 @@
 import React from "react";
+import { Button } from '@mui/material';
+import PersistentDrawerLeft from "../components/PersistentDrawerLeft";
 
 const Index = () => {
     return (
-        <div>
-            Main page
-        </div>
+        <>
+            <PersistentDrawerLeft />
+
+            <div className="center">
+                <h1>Добро пожаловать</h1>
+                <h3>Здесь собраны лучшие треки!</h3>
+            </div>
+
+            <style jsx>
+                {`
+                    .center {
+                        margin-top: 150px;
+                        display: flex;
+                        flex-direction: column;
+                        align-items: center;
+                        justify-content: center;
+                    }
+                `}
+            </style>
+        </>
+
     );
 };
 
