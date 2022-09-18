7818c928afa72a90609f74fb696e2fd2db960a38
Matvii Voznosimenko <main.monyster@gmail.com>
2022-09-18 18:50:45 +0300

    commit #1

---
+++ server/src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from "@nestjs/common";
+import { MongooseModule } from '@nestjs/mongoose';
+import { FileModule } from "./file/file.module";
+import { TrackModule } from "./track/track.module";
+
+
+
+@Module({
+    imports: [
+        MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.5jdk08p.mongodb.net/?retryWrites=true&w=majority'),
+        TrackModule,
+        FileModule]
+})
+export class AppModule { }
